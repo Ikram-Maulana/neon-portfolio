e521a3739c983a99dd93248010b2dbf0217f839e
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-23 21:25:20 +0700

    chore(deps): delete google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GoogleAnalytics trackPageViews />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
