311d22ad558f150cbf416183681dc8a61201be5d
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-23 22:29:07 +0700

    chore(analytics): add umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://analytics.ikrammaulana.my.id/script.js"
+        strategy="lazyOnload"
+        data-website-id="c27f1d9d-4f96-4854-8ac9-a2f9bd9b043c"
+      />
+    </>
+  );
 }
 
 export default MyApp;
