03200f896bc5e7c9299059625aa4d0ac8c5f1afa
Ikram Maulana <ikram075@ummi.ac.id>
2024-02-03 07:02:53 +0700

    chore(anaytics): update umami analytics scripts
    - resync env
    - update umami analytics scripts to dynamic on env

--- src/env.js
+++ src/env.js
@@ -6,10 +6,11 @@ export const env = createEnv({
     API_URL: z.string().url(),
   },
   client: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_UMAMI_URL: z.string().url(),
+    NEXT_PUBLIC_UMAMI_WEBSITE_ID: z.string(),
   },
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_UMAMI_URL: process.env.NEXT_PUBLIC_UMAMI_URL,
+    NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
   },
 });
